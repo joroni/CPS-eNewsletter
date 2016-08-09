d0c18a8cfd70dd1723ac9981851444e3b3b7dde0
kophonegap <niconi@coca-cola.com>
2016-08-09 18:39:25 +0800

    files

---
+++ www/config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'cps-enewsletter',
+		wp_ws_url : 'http://104.238.96.209/~newsimtms/wordpress/wp-appkit-api/cps-enewsletter',
+		wp_url : 'http://104.238.96.209/~newsimtms/wordpress',
+		theme : 'cps-newsletter',
+		version : '1.0',
+		app_title : 'CPS e-Newsletter',
+		app_platform : 'ios',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : '$bh3twPo`@hdStW9pva6;i;VLByX~F{ZZJ-/a%q?pa]Fp%Qbgz!<obRboQUST}Se',
+		options : {"refresh_interval":59},
+		theme_settings : [],
+		addons : []
+	};
+
+});
